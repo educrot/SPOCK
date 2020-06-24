5f22d5e60c874e555efa742aa69459eabca3ad05
Elsa Ducrot <educrot@uliege.be>
2020-06-24 10:57:27 +0200

    commit 24062020d

---
+++ SPOCK/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'SPOCK',
+    version = '0.0.0',
+    author = 'Elsa Ducrot',
+    author_email = 'educrot@uliege.be',
+    description = ('Speculoos Observatory SChedule maKer'),
+    keywords = '',
+    url = 'https://github.com/educrot/SPOCK/',
+    packages = find_packages(),
+    long_description = read('README.rst'),
+    install_requires = [
+    ],
+)
