ed2042c9d3cfab177768f0b58d159bfb18c601f3
Elsa Ducrot <educrot@uliege.be>
2020-09-28 18:11:12 +0200

    20200928

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SPOCK", # Replace with your own username
+    version="0.0.1",
+    author="Elsa Ducrot",
+    author_email="educrot@uliege.be",
+    long_description = read('README.rst'),
+    install_requires = ['pandas','numpy','astroplan','astropy','matplotlib','datetime','pyaml','docx','plotly'],
+    install_requires = ['pandas','numpy==1.19.0','astroplan','astropy','matplotlib','datetime','pyaml','docx','plotly'],
+    url = 'https://github.com/educrot/SPOCK/',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
